e5ba98165877fcbce96e8bdd6e20c65b0706b3c4
StephenHuang3 <91025265+StephenHuang3@users.noreply.github.com>
2022-12-06 16:03:17 -0500

    removed useless param and func

--- ability.h
+++ ability.h
@@ -11,8 +11,6 @@
 // may need to change to #include Link if we're using functions of Link
 
 class Ability {
-  std::string name;
-  int uses;
 
   public:
     // activates ability, if needed, we can pass a pointer to the board
@@ -21,7 +19,6 @@ class Ability {
     // checks what kind and how much information we need to read in;
     virtual char checkInput() = 0;
 
-    int getUses();
     virtual std::string getName() = 0;
   };
 
